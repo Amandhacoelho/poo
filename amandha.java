f7929115f7d2e75ce69a5b923129d1c5c70e5336
Amandhacoelho <135068790+Amandhacoelho@users.noreply.github.com>
2023-06-12 18:20:56 -0300

    Update amandha.java

--- amandha.java
+++ amandha.java
@@ -1,33 +1,50 @@
-Pessoa p1 = new Pessoa ();
-p1.nome = "maria";
-p1.idade = 18; 
-p1.sexo = 'F';
-p1.cpf = "123.456.789-00";
-
-
-
-Pessoa p2 = new ´Pessoa ();
-p2.nome = "Joao";
-p2.idade = 22;
-p2.cpf = 'M'
-p2.cpf = "234.567.890-00";
-
-
-Conta c1 = new Conta ();
-c1.numero = "1234-5";
-c1.titular = p1; // a Pessoa p1 eh titular desta Conta!
-c1.saldo = 100.0;
-c1.limite = 200.0;
-    
-	
-Conta c2 = new conta ();
-c2.numero = "2345-6"; // esta Conta pertence 'a pessoa p2
-c2.titular = p2;
-c2.saldo = 150.0;
-c2.limite = 200.0;
-
-
-System.out.println(c1.saldo);
-System.out.println(c1.titular.nome);
-System.out.println("Saldo da conta " + c2.numero + " ; " +  c2.saldo);
-
+class Pessoa{
+  String nome;
+  int idade;
+  char sexo;
+  String cpf; 
+}
+class Conta{
+  String numero;
+  Pessoa titular;
+  double saldo;
+  double limite;
+}
+public class Main {
+  public static void main(String[] args) {
+            Pessoa p1 = new Pessoa ();
+        p1.nome = "maria";
+        p1.idade = 18; 
+        p1.sexo = 'F';
+        p1.cpf = "123.456.789-00";
+        
+        
+        
+        Pessoa p2 = new Pessoa ();
+        p2.nome = "Joao";
+        p2.idade = 22;
+        p2.sexo = 'M';
+        p2.cpf = "234.567.890-00";
+        
+        
+        Conta c1 = new Conta ();
+        c1.numero = "1234-5";
+        c1.titular = p1; // a Pessoa p1 eh titular desta Conta!
+        c1.saldo = 100.0;
+        c1.limite = 200.0;
+        
+        
+        Conta c2 = new Conta ();
+        c2.numero = "2345-6"; // esta Conta pertence 'a pessoa p2
+        c2.titular = p2;
+        c2.saldo = 150.0;
+        c2.limite = 200.0;
+        
+        
+        System.out.println(c1.saldo);
+        System.out.println(c1.titular.nome);
+        System.out.println("Saldo da conta " + c2.numero + ";" +  c2.saldo);
+        
+            System.out.println("Hello world!");
+          }
+        }
